5df3302e9892a8a545cb9e9c155eb94abfeaecab
David Weaver <david.weaver@digitalbridge.com>
2020-03-03 05:26:51 +0000

    add: movement and collision detection

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::cmp::{max, min};
 #[macro_use]
 extern crate specs_derive;
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -27,6 +32,10 @@ impl State {
         lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
 }
 
 #[derive(Component, Debug)]
@@ -53,9 +62,14 @@ impl<'a> System<'a> for LeftWalker {
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        
+
+
         player_input(self, ctx);
         self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+        
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -66,13 +80,18 @@ impl GameState for State {
     }
 }
 
+// a system
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
         pos.x = min(79 , max(0, pos.x + delta_x));
         pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -90,6 +109,60 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+
 fn main() {
 
     use rltk::RltkBuilder;
@@ -100,6 +173,9 @@ fn main() {
     let mut gs = State {
         ecs: World::new()
     };
+
+    // add map
+    gs.ecs.insert(new_map());
     
     // register our components with the ECS
     gs.ecs.register::<Position>();
